d36d9332e5a88222f7c4b700dcee1b9dc044ba4b
Ricardo García Ramírez <rgr.5882@gmail.com>
2024-09-15 11:05:34 -0600

    Add more cities to search

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from process_ds_jobs import run_ds_daily_scraper
+from src.process_ds_jobs import run_ds_daily_scraper
 from Utils.logger import Logger
+import pandas as pd
 
 if __name__ == '__main__':
+    cities = ['Monterrey', 'Guadalajara', 'Mexico City']
+
     logger = Logger('main.log')
-    logger.log.info('Initializing web scraping for LinkedIn Jobs.')
-    file = 'LinkedIn_Jobs_Data_Scientist_Monterrey.csv'
-    run_ds_daily_scraper(logger=logger, file_name=file)
+
+    logger.log.info(f'Initializing web scraping for LinkedIn Jobs for the cities {cities}.')
+
+    for city in cities:
+        city_filename = city.replace(" ", "_")
+        file = f'LinkedIn_Jobs_Data_Scientist_{city_filename}.csv'
+        run_ds_daily_scraper(logger=logger, location=city, file_name=file)
+
+    df_mty = pd.read_csv('LinkedIn_Jobs_Data_Scientist_Monterrey.csv')
+    df_gdl = pd.read_csv('LinkedIn_Jobs_Data_Scientist_Guadalajara.csv')
+    df_cdmx = pd.read_csv('LinkedIn_Jobs_Data_Scientist_Mexico_City.csv')
+
+    df_jobs_all = pd.concat([df_mty, df_gdl, df_cdmx], ignore_index=True)
+    df_jobs_all.to_csv('LinkedIn_Jobs_Data_Scientist_Mexico.csv', index=False)
