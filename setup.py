823a1c987a7403551f4e99792aa23f53530a95c3
Ricardo García Ramírez <rgr.5882@gmail.com>
2024-09-15 00:11:16 -0600

    Setup for pip install

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='LinkedInWebscraper',
-    version='0.1',
+    version='0.2',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
-        # Placeholder
+        'requests',
+        'beautifulsoup4',
+        'pandas',
+        'numpy',
+        'openai', 
+        'python-dotenv'
     ],
 )
